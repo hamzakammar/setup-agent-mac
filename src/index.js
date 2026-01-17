89524c50c86bd04bb63a7ad7828a29a8bf6580e2
Hamza <hamza.k.ammar@gmail.com>
2026-01-17 10:08:01 -0500

    Added node tarball for bundled node

---
+++ src/index.js
@@ -0,0 +1,38 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+
+function createWindow() {
+  // Create the browser window.
+  const mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    }
+  });
+
+  // Load the index.html file
+  mainWindow.loadFile('src/index.html');
+
+  // Open the DevTools (optional)
+  // mainWindow.webContents.openDevTools();
+}
+
+// This method will be called when Electron has finished initialization
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+// Quit when all windows are closed, except on macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
